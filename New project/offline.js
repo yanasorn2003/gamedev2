21553d9cfec48a1357710da20c8cbe3ae9c5ec07
yanasorn2003 <119282971+yanasorn2003@users.noreply.github.com>
2024-08-01 12:08:41 +0700

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1722488749,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/movingsolidtile.png",
+		"images/jumpthrutile.png",
+		"images/monster-sheet0.png",
+		"media/elden ring main theme (the final battle) ｜ epic version.m4a",
+		"media/elden ring main theme (the final battle) ｜ epic version.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
